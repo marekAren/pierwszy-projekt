f77e57abbbc1997887be31b17f3a42b7b97d1542
marekAren <marekf555@interia.pl>
2023-10-01 19:33:02 +0200

    tylko index js 3 param tylko w for

--- JS_test/app.js
+++ JS_test/app.js
@@ -12,9 +12,9 @@ const people = [
 // console.log("🚀 ~ file: index.html:20 ~ person:", person);
 // console.log(people[3]);
 
-const personDisplay = (person) => {
+const personDisplay = (person, index, arr) => {
     // console.log(person.name, person.lastname, person.country);
-    console.log(`${person.name} ${person.lastname}, kraj: ${person.country}`);
+    console.log(`${index+1} z ${arr.length }: ${person.name} ${person.lastname}, kraj: ${person.country}`);
 }
 
 for (let i=0; i < people.length; i++ ) {
@@ -22,5 +22,25 @@ const person = people[i];
     // console.log(people[i]);
 //    console.log(`${person.name} ${person.lastname}, kraj: ${person.country}`);
 
-    personDisplay(person);
+    personDisplay(person, i, people);
 };
+
+console.log('#forEach#');
+
+    // console.log(people.forEach(pozycja));
+    // people.forEach((person_linii) => {
+
+people.forEach ((person_linii,index) => {
+    // console.log(person_linii);
+    personDisplay(person_linii,index);
+
+});
+
+// skrot cmd+alt+strzalka gora/dol pisanie w wielu liniach na raz 
+
+// console.log(people[0],1,people)
+// console.log(people[1],2,people)
+// console.log(people[2],3,people)
+// console.log(people[3],4,people)
+// console.log(people[4],5,people)
+
